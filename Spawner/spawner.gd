93341861351e376be04fd1bfa5fb21f3e730c0e2
GregOlszyk <gregolszyk1@gmail.com>
2025-06-10 18:26:23 +0200

    Implemented Dynamic Spawn and Walls

--- Spawner/spawner.gd
+++ Spawner/spawner.gd
@@ -4,10 +4,11 @@ extends Node2D
 @export var spawn_interval := 1.0
 @export var spawn_area_width := 400
 @export var y_position := 0
+var timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var timer = Timer.new()
+	timer = Timer.new()
 	timer.wait_time = spawn_interval
 	timer.autostart = true
 	timer.one_shot = false
@@ -21,3 +22,8 @@ func _on_timer_timeout() -> void:
 	var x_position = randf_range(-spawn_area_width / 2, spawn_area_width / 2)
 	obj.position = Vector2(x_position, y_position)  # Start above screen
 	get_tree().current_scene.add_child(obj)
+
+
+func _on_spawn_rate_timer_timeout() -> void:
+	spawn_interval -= 0.10
+	timer.wait_time = spawn_interval
